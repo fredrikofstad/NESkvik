ffbdee2317e3358a625f2ce9928b236ff8d80e0a
fredrikofstad <fredrik.ofstad@gmail.com>
2025-07-02 11:35:29 +0200

    updated stub files in testing

--- tests/stub_ppu.cpp
+++ tests/stub_ppu.cpp
@@ -1,6 +1,10 @@
 #include "../core/PPU/ppu.h"
 
+PPU::PPU() {}
 void PPU::mainBusWrite(uint16_t, uint8_t) { }
 uint8_t PPU::mainBusRead(uint16_t, bool) const { return 0x00; }
 void PPU::attachRom(std::shared_ptr<ROM>) {}
 void PPU::clock() {}
+void PPU::reset() {}
+
+
